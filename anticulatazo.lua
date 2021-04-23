b78b44bc71ead2bce6e8c08dd8d5c903a7ff6976
lizarragaeneko <67381650+lizarragaeneko@users.noreply.github.com>
2021-04-23 20:06:31 +0200

    Create anticulatazo.lua

---
+++ anticulatazo.lua
@@ -0,0 +1,14 @@
+print("###################################")
+print("KAMIKAZELEAKS - ElNekoDev")
+print("###################################")
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        DisableControlAction(1, 140, true)
+        if IsPlayerFreeAiming(PlayerId()) then
+            DisableControlAction(1, 141, true)
+            DisableControlAction(1, 142, true)
+        end
+    end
+end)
